683ce1f237528c53b24aed6dfa28c237f6471021
Rafael <rafaeldavi8086@gmail.com>
2025-01-20 21:20:24 -0300

    Exercício fixação - Lista encadeada

---
+++ lista-encadeada/Exercicios-Fixacao/fix1/linkedList.h
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include "node.h"
+
+typedef struct simple_linked_list* List;
+
+
+List createList();
+
+void destroyList(List list);
+
+bool insertList(List list, Info element);
+
+Info getInfoAt(List list, int position);
+
+bool isEmptyList(List list);
+
+int size(List list);
+
+bool insertAtBeginningList(List list, Info element);
+
+bool insertAtPositionList(List list, int position, Info element);
+
+bool hasElement(List list, Info element);
+
+bool removeAt(List list, int position);
+
+bool countKey(List list, Info key);
